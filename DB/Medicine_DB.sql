432c05ea5cdc44cfcc9fa3225faf2f97751104c2
Anastasia <105925231+Morozova22@users.noreply.github.com>
2022-07-19 21:35:06 +0800

    Add files via upload

--- DB/Medicine_DB.sql
+++ DB/Medicine_DB.sql
@@ -10,8 +10,9 @@ CREATE TABLE IF NOT EXISTS `medicine`.`Раздел` (
   `заголовок` VARCHAR(45) NULL,
   PRIMARY KEY (`код_раздела`));
 
+
 -- -----------------------------------------------------
--- Таблица Товар
+--  Таблица Товар
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `medicine`.`Товар` (
   `код_товара` INT NOT NULL AUTO_INCREMENT,
@@ -20,20 +21,19 @@ CREATE TABLE IF NOT EXISTS `medicine`.`Товар` (
   `цена_без_скидки` DECIMAL(10,2) NOT NULL,
   `цена_по_промокоду` DECIMAL(10,2) NOT NULL,
   `описание` VARCHAR(300) NOT NULL,
-  `картинка_анонс` VARCHAR(255) NOT NULL,
-  `доп_картинка` VARCHAR(255) NOT NULL,
   `заголовок` VARCHAR(45) NULL,
   PRIMARY KEY (`код_товара`));
 
+
 -- -----------------------------------------------------
--- Промежуточная таблица Раздел - Товар
+-- Промежуточная таблица Раздел-Товар
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `medicine`.`Раздел_has_Товар` (
   `код_раздела` INT NOT NULL,
   `код_товара` INT NOT NULL,
   PRIMARY KEY (`код_раздела`, `код_товара`),
   INDEX `Раздел_has_Товар_Товар` (`код_товара`),
-  INDEX `fk_Раздел_has_Товар_Раздел` (`код_раздела`),
+  INDEX `Раздел_has_Товар_Раздел` (`код_раздела`),
   CONSTRAINT `Раздел_has_Товар_Раздел`
     FOREIGN KEY (`код_раздела`)
     REFERENCES `medicine`.`Раздел` (`код_раздела`)
@@ -44,3 +44,21 @@ CREATE TABLE IF NOT EXISTS `medicine`.`Раздел_has_Товар` (
     REFERENCES `medicine`.`Товар` (`код_товара`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+--  Таблица Картинка
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `medicine`.`Картинка` (
+  `код_картинки` INT NOT NULL AUTO_INCREMENT,
+  `адрес_картинки` VARCHAR(255) NOT NULL,
+  `код_товара` INT NOT NULL,
+  PRIMARY KEY (`код_картинки`),
+  INDEX `Картинка_Товар` (`код_товара`),
+  CONSTRAINT `Картинка_Товар`
+    FOREIGN KEY (`код_товара`)
+    REFERENCES `medicine`.`Товар` (`код_товара`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+
